dcc83686daa3a6b667437a641d252ba09f5c1cf3
ThePokePreston <prestonphung@gmail.com>
2023-07-08 19:41:53 -0600

    BuyMenuchanges
    dasdjf

--- Assets/BuyMenu.cs
+++ Assets/BuyMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,15 +8,19 @@ public class BuyMenu : MonoBehaviour {
 
     UIDocument buttonDocument;
     Button uiButton;
+    VisualElement vElement;
+    public Sprite sprite;
+    VisualElement room1;
 
     void OnEnable() {
         buttonDocument = GetComponent<UIDocument>();
+        room1 = buttonDocument.rootVisualElement.Q("Room1") as VisualElement;
 
         if (buttonDocument == null ) {
             Debug.LogError("No Button document found");
         }
 
-        uiButton = buttonDocument.rootVisualElement.Q("skeleton") as Button;
+        uiButton = buttonDocument.rootVisualElement.Q("SkeletonButton") as Button;
 
         if(uiButton != null) {
             Debug.Log("Button Found");
@@ -26,6 +31,15 @@ public class BuyMenu : MonoBehaviour {
 
     public void OnButtonClick(ClickEvent evt) {
         Debug.Log("The UI Button has been clicked on");
+
+        if(vElement == null) {
+            Debug.LogError("No VisualElement Found");
+        }
+        room1.RegisterCallback<ClickEvent>(OnRoom1Clicked);
+    }
+
+    public void OnRoom1Clicked(ClickEvent evt) {
+        room1.style.backgroundImage = new StyleBackground(sprite);
     }
 
     public void PurchaseSkeleton() {
