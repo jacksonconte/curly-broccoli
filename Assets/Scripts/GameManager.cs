b939847cafecab8a66a3c1184ea57f4eeb5aec14
ThePokePreston <prestonphung@gmail.com>
2023-07-08 20:47:39 -0600

    sdf
    sdf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState { BUYPHASE, COMBATPHASE }
+public enum GameState { MAINMENU, BUYPHASE, COMBATPHASE, TRAPPHASE, PAUSEMENU}
 
 public class GameManager : MonoBehaviour
 {
