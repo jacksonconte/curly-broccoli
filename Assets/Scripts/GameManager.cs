35d7ddf22f39cd850e8e4f7c4881c3ab30bc8c0b
ThePokePreston <prestonphung@gmail.com>
2023-07-08 21:29:32 -0600

    PauseMenu Escape functionality
    Escape key to pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public enum GameState { MAINMENU, BUYPHASE, COMBATPHASE, TRAPPHASE, PAUSEMENU}
 
 public class GameManager : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
     public Array rooms = new Array[7];
     public int money;
     public int round = 0;
@@ -19,6 +21,43 @@ public class GameManager : MonoBehaviour
         state = GameState.BUYPHASE;
         BuyPhase();
     }
+
+    // Pause Menu
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(GameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+    
+    // Resumes Game
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    // Pauses Game
+    void Pause() {
+        if (GameIsPaused) {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+    }
+
+    public void LoadMenu() {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quiting Game...");
+    }
+    
 
     void BuyPhase()
     {
