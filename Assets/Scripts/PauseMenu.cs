5ad522bcc967998b2295cbfb696f7fb15993789f
ThePokePreston <prestonphung@gmail.com>
2023-07-08 21:48:50 -0600

    pausemenu started
    sdf

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    Button Resume;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
